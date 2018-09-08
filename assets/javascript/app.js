a3dc676b9ac93eb6ea9594b858d2da281a0063ed
Raynor <rfisher708@me.com>
2018-09-07 19:57:47 -0500

    Train

---
+++ assets/javascript/app.js
@@ -0,0 +1,51 @@
+
+var trainName = "";
+var trainDestination = "";
+var trainTime = "";
+var trainFrequency = "";
+var nextArrival = "";
+var minutesAway = "";
+var elTrain = $("#train-name");
+var elTrainDestination = $("#train-destination");
+var elTrainTime = $("#train-time").mask("00:00");
+var elTimeFreq = $("#time-freq").mask("00");
+    var trainDiff = 0;
+    var trainRemainder = 0;
+    var minutesTillArrival = "";
+    var nextTrainTime = "";
+    var frequency = snapshot.val().frequency;
+
+    trainDiff = moment().diff(moment.unix(snapshot.val().time), "minutes");
+
+    trainRemainder = trainDiff % frequency;
+
+    minutesTillArrival = frequency - trainRemainder;
+    nextTrainTime = moment().add(minutesTillArrival, "m").format("hh:mm A");
+
+    $("#table-data").append(
+        "<tr><td>" + snapshot.val().name + "</td>" +
+        "<td>" + snapshot.val().destination + "</td>" +
+        "<td>" + frequency + "</td>" +
+        "<td>" + minutesTillArrival + "</td>" +
+        "<td>" + nextTrainTime + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
+    );
+
+    $("span").hide();
+
+var storeInputs = function(event) {
+
+    event.preventDefault();
+
+    trainName = elTrain.val().trim();
+    trainDestination = elTrainDestination.val().trim();
+    trainTime = moment(elTrainTime.val().trim(), "HH:mm").subtract(1, "years").format("X");
+    trainFrequency = elTimeFreq.val().trim();
+    
+    alert("Train Added");
+
+    elTrain.val("");
+    elTrainDestination.val("");
+    elTrainTime.val("");
+    elTimeFreq.val("");
+};
+
